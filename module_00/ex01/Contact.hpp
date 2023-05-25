c2e4cd5c9217d871cd8c7c3d9f0483e7d8c9cd16
timothee <tmerciercodam@gmail.com>
2023-05-25 08:59:14 +0200

    ex01

--- module_00/ex01/Contact.hpp
+++ module_00/ex01/Contact.hpp
@@ -5,19 +5,26 @@
 
 class Contact {
 private:
-  std::string firstName;
-  std::string lastName;
-  std::string nickName;
+  std::string fName;
+  std::string lName;
+  std::string nName;
+  std::string phone;
+  std::string secret;
 
   // creation of a constructor Contact with
   // an initializer list -> initiates directly
   // the class members when they will be created
 public:
-    Contact() : firstName(""), lastName(""), nickName(""){};
-    Contact(std::string fName, std::string lName, std::string nName)
-        : firstName(fName), lastName(lName), nickName(nName){};
+  Contact() : fName(""), lName(""), nName(""){};
+  Contact(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
+      : fName(s1), lName(s2), nName(s3), phone(s5), secret(s4){};
   std::string getFirstName();
   std::string getLastName();
   std::string getNickName();
+  std::string getPhone();
+  std::string getSecret();
   std::string getInput(std::string);
+  bool checkEmpty(int);
+  void displayPb();
+
 };
--- module_00/ex01/PhoneBook.hpp
+++ module_00/ex01/PhoneBook.hpp
@@ -2,16 +2,18 @@
 
 #include "Contact.hpp"
 #include <iostream> // For std::cout
-#include <string>   // For std::string
+#include <string>   // For std::string#include <cstdlib>
+#include <iomanip> 
 
 class PhoneBook {
 private:
   Contact contacts[8];
 
 public:
+  
   Contact *addContact(int i);
-  void fillBook(int);
+  void fillPb(int);
   void searchContact();
-  void displayContact(std::string);
   void prompt();
+  // bool checkEmpty(Contact);
 };
